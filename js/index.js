26031932c02d1e86763edac4e334aea3cd3ab487
Johnny Valles <vallesja@uci.edu>
2022-10-10 08:36:18 -0700

    add particles js to contact section

--- js/index.js
+++ js/index.js
@@ -30,3 +30,8 @@ siteLinks.forEach(link => {
     toggleMenu();
   });
 });
+
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', 'particlesjs-config.json', function() {
+  console.log('callback - particles.js config loaded');
+});
